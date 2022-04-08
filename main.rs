6bca6842ddfdeccaf80f90c4d9627b3481a54982
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-04-08 16:41:11 -0300

    Parte 4 da aula 2 do curso Rust: a linguagem de programação performática e segura

--- main.rs
+++ main.rs
@@ -1,3 +1,6 @@
+const PI: f32 = 3.14;
+static VARIAVEL_GLOBAL: i8 = -1;
+
 fn main()
 {
     let variavel: u8 = 128;
@@ -14,4 +17,8 @@ fn main()
 
     let letra: char = 'z';
     println!("letra = {}, tamanho = {}", letra, std::mem::size_of_val(&letra));
+
+    println!("PI = {}, tamanho = {}", PI, std::mem::size_of_val(&PI));
+
+    println!("VARIAVEL_GLOBAL = {}, tamanho = {}", VARIAVEL_GLOBAL, std::mem::size_of_val(&VARIAVEL_GLOBAL))
 }
