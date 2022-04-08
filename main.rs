9b6d2f3c7dd638e937ad2d35ad0ebbc461072b64
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-04-08 10:33:39 -0300

    Parte 2 da aula 2 do curso Rust: a linguagem de programação performática e segura

--- main.rs
+++ main.rs
@@ -1,4 +1,17 @@
 fn main()
 {
-    println!("Ola mundo!");
+    let variavel: u8 = 128;
+    println!("variavel = {}, tamanho = {}", variavel, std::mem::size_of_val(&variavel));
+
+    let decimal: f32 = 2.5;
+    println!("decimal = {}, tamanho = {}", decimal, std::mem::size_of_val(&decimal));
+
+    let mut booleana: bool = false;
+    println!("booleana = {}, tamanho = {}", booleana, std::mem::size_of_val(&booleana));
+
+    booleana = true;
+    println!("booleana = {}, tamanho = {}", booleana, std::mem::size_of_val(&booleana));
+
+    let letra: char = 'z';
+    println!("letra = {}, tamanho = {}", letra, std::mem::size_of_val(&letra));
 }
